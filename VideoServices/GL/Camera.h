cba2ae84e9dcec5608137c6555572a2821034005
Paul Spiteri <paulspiteri@hotmail.com>
2025-05-05 11:01:40 -0400

    simplify

--- VideoServices/GL/Camera.h
+++ VideoServices/GL/Camera.h
@@ -2,14 +2,13 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
 
 class Camera
 {
     glm::vec3 position;
     glm::vec3 front;
     glm::vec3 up;
-    float yaw;
-    float pitch;
     float fov;
 
 public:
@@ -17,31 +16,37 @@ public:
         position(glm::vec3(0.0f, 0.0f, 60.0f)),
         front(glm::vec3(0.0f, 0.0f, -1.0f)),
         up(glm::vec3(0.0f, 1.0f, 0.0f)),
-        yaw(0.0f),
-        pitch(0.0f),
         fov(fovDegrees)
     {
     }
 
     glm::mat4 getViewMatrix() const
     {
+        std::cout << "Camera pos: " << position.x << ", " << position.y << ", " << position.z << "\n";
+        std::cout << "Front: " << front.x << ", " << front.y << ", " << front.z << "\n";
+        std::cout << "Up: " << up.x << ", " << up.y << ", " << up.z << "\n";
+
         return glm::lookAt(position, position + front, up);
     }
 
     glm::mat4 getProjectionMatrix(float aspectRatio) const
     {
-        return glm::perspective(glm::radians(fov), aspectRatio, 0.1f, 10000000.0f);
-       // return glm::perspective(glm::radians(fov), aspectRatio, 10.0f, 100000000.0f);
+        const float nearPlane = 100.0f;
+        const float farPlane = 1000000.0f;
+
+        return glm::perspective(
+            glm::radians(fov),
+            aspectRatio,
+            nearPlane,
+            farPlane
+        );
     }
 
     glm::vec3 getPosition() const { return position; }
     glm::vec3 getFront() const { return front; }
     float getFov() const { return fov; }
 
-    void setPosition(const glm::vec3& pos)
-    {
-        position = pos;
-    }
+    void setPosition(const glm::vec3& pos) { position = pos; }
     //
     // void moveForward(float distance)
     // {
@@ -59,15 +64,10 @@ public:
     //     position += up * distance;
     // }
 
-    void rotate(float newYaw)
+    void rotate(float yaw)
     {
-        yaw = newYaw;
-
-        // Update front vector
-        glm::vec3 direction;
-        direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-        direction.y = sin(glm::radians(pitch));
-        direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-        front = glm::normalize(direction);
+        front.x = cos(glm::radians(yaw));
+        front.y = 0;
+        front.z = sin(glm::radians(yaw));
     }
 };
